96dba2384d6bebf596f3010ec8200792e1debf16
Jetro Roa <jetrotullroa@gmail.com>
2024-10-01 14:37:21 -0400

    fix page

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,7 +1,7 @@
 export default function Tours() {
 return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        This is a tour page
+        This is a tour pagez
 </div>
 )
 }
